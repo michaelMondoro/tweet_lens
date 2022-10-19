2c32c196d1dd27697236d06aa49ca95759db8c5e
Michael Mondoro <michaelmondoro@gmail.com>
2022-10-19 14:57:33 -0400

    cleanup

--- twit.py
+++ twit.py
@@ -7,9 +7,16 @@ from TwitAnalyzer import TwitAnalyzer
 # ------
 # NOTES
 # ------
+
 # Search for tweets
 # query_results = api.search_tweets(q="",tweet_mode="extended") # ONLY searches within past 7 days
 # geo_results = api.search_geo(lat,lon,tweet_mode="extended")
+
+# Twitter search operators
+# https://developer.twitter.com/en/docs/twitter-api/v1/rules-and-filtering/search-operators
+
+
+# Time Formatting
 #tweet.created_at.astimezone().strftime('%m/%d/%Y - %I:%M %p')
 
 total_tweets = 0
