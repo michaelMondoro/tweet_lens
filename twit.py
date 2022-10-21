a346faec669674625821dfb211dfbcb53ab75efc
Michael Mondoro <michaelmondoro@gmail.com>
2022-10-21 17:05:43 -0400

    updated test file

--- twit.py
+++ twit.py
@@ -57,6 +57,7 @@ def trend_stats(location, num_trends, live):
     trends = a.get_trends(a.trend_locations[location]["woeid"])
     data={}
     total_tweets = 0
+    total_reg_tweets = 0
     total_retweets = 0
     total_unique_retweets = 0
     total_volume = 0
@@ -80,37 +81,38 @@ def trend_stats(location, num_trends, live):
         thread.join()
 
         total_tweets += streem.tweets
+        total_reg_tweets += streem.reg_tweets
         total_retweets += streem.retweets
         total_unique_retweets += streem.get_unique_retweets()
         total_volume += trend['tweet_volume']
         
-        data[trend['name']] = { 'tweets':streem.tweets ,
+        data[trend['name']] = { 'tweets':streem.tweets,
+                                'reg_tweets':streem.reg_tweets,
                                 'retweets':streem.retweets, 
                                 'unique_retweets':streem.get_unique_retweets(),
+                                'perc_retweets':streem.get_perc_retweets(),
+                                'perc_unique_retweets':streem.get_perc_unique_retweets(),
                                 'tw_p_min': streem.tweets*2}
 
 
     # Create results table
-    table = PrettyTable(['Trend', 'Tweets', 'Retweets', 'Unique Retweets', 'twt/min', '% Retweets', '% Unique Retweets'])
+    table = PrettyTable(['Trend', 'Total Tweets', 'Regular Tweets', 'Retweets', 'Unique Retweets', 'twt/min', '% Retweets', '% Unique Retweets'])
     table.set_style(SINGLE_BORDER)
     table.align = 'l'
 
     for trend in data:
-        perc_retweet = 0
-        if data[trend]['tweets'] > 0:
-            perc_retweet = round((data[trend]['retweets']/data[trend]['tweets'])*100,2)
-        table.add_row([trend, data[trend]['tweets'], data[trend]['retweets'], data[trend]['unique_retweets'], data[trend]['tw_p_min'], perc_retweet, round((data[trend]['unique_retweets']/data[trend]['retweets'])*100,2)])
-    table.add_row(['Summary', total_tweets, total_retweets, total_unique_retweets, round(total_tweets/(num_trends/2)), round((total_retweets/total_tweets)*100,2), round((total_unique_retweets/total_retweets)* 100,2)])    
+        table.add_row([trend, data[trend]['tweets'], data[trend]['reg_tweets'], data[trend]['retweets'], data[trend]['unique_retweets'], data[trend]['tw_p_min'], data[trend]['perc_retweets'], data[trend]['perc_unique_retweets']])
+    table.add_row(['Summary', total_tweets, total_reg_tweets, total_retweets, total_unique_retweets, round(total_tweets/(num_trends/2)), round((total_retweets/total_tweets)*100,2), round((total_unique_retweets/total_retweets)* 100,2)])    
 
     print("\n")
     print(f"Summary of top {num_trends} trends from [ {colored(location,'magenta')} ]")
     print(table)
-    print(f"\nProcessed {round((total_tweets/total_volume)*100,4)}% of total volume")
-    print(f"[{total_tweets-total_retweets} regular ] [ {total_retweets} retweets ] [ {total_unique_retweets} unique retweets ]")
+    print(f"\nProcessed {round((total_tweets/total_volume)*100,4)}% of total volume - [ {total_tweets:,} tweets ]")
+    print(f"[{total_reg_tweets} regular ] [ {total_retweets} retweets ] [ {total_unique_retweets} unique retweets ]")
     print("\n")
 
 if __name__ == "__main__":
     a = TwitAnalyzer()
 
-    trend_stats("Ukraine", 3, False)
+    trend_stats("Worldwide", 3, False)
     
