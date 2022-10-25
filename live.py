eb9075d0488d4b409b746a52d4363f2fe4b86397
Michael Mondoro <michaelmondoro@gmail.com>
2022-10-25 11:34:11 -0400

    added outline for live processing module

---
+++ live.py
@@ -0,0 +1,21 @@
+from TwitAnalyzer import TwitAnalyzer
+from TwitStream import TwitStream
+
+'''
+Module for processing live twitter data
+'''
+class Live:
+	a = TwitAnalyzer()
+
+	# Create and start a Twitter stream
+	def stream(query, live):
+	    twit_stream = TwitStream(a.config['CONSUMER_KEY'],a.config['CONSUMER_SECRET'],a.config['ACCESS_TOKEN'],a.config['ACCESS_TOKEN_SECRET'], live=live)
+	    thread = twit_stream.filter(track=[query], stall_warnings=True, threaded=True)
+		return twit_stream, thread
+
+	def TrendAnalysis(self):
+		pass
+
+
+	def SearchAnalysis(self):
+		pass
