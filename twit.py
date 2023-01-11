1030630c6b54473d2f97f5a296456d07319b1734
Michael M <michaelmondoro@gmail.com>
2023-01-10 22:15:58 -0500

    updated to be importable, and connect to mongoDB

--- twit.py
+++ twit.py
@@ -1,6 +1,4 @@
-import tweepy
-import yaml
-import redis
+import pymongo
 from TwitStream import TwitStream
 from TwitAnalyzer import TwitAnalyzer
 from TwitLive import TwitLive
@@ -39,15 +37,85 @@ def get_hrs_mins(seconds):
     return (hrs,mins,seconds)
 
 
+# Calculate sentiment
+def calculate_sentiment(trend, cur_pos, cur_neg, cur_total):
+    pos = cur_pos + trend.pos 
+    neg = cur_neg + trend.neg 
+    total = cur_total + trend.tweets 
 
-if __name__ == "__main__":
+    return ( round(pos/total*100,2), round(neg/total*100,2) )
+
+'''
+# Trend dictionary
+{
+    "name": trend_name,
+    "volume": volume,
+    "tweets": tweets,
+    "reg_tweets": reg_tweets,
+    "retweets": retweets,
+    "positive_tweets": positive_tweets,
+    "negative_tweets": negative_tweets,
+    "sentiment": sentiment,
+    "impact": impact,
+    "top_users": [(user,num_tweets),(user,num_tweets)],
+    "top_locations": [(location,num_tweets),(location,num_tweets)]
+}
+
+# increment db field by 1
+trends.update_one({'field':value},{"$inc": {'field_to_increment':1}})
+# update value to new value
+trends.update_one({'field':value},{"$set": {'field':new_value}})
+
+db.list_collection_names()
+
+
+'''
+
+def process():
+    # Connect to mongoDB
+    client = pymongo.MongoClient()
+    db = client.tweet_lens_db
+    trends = db.trends 
+
+    # Get current list of trends in DB
+    trend_list = [trend['name'] for trend in trends.find()]
+
+    # Set up Twitter processing
     a = TwitAnalyzer()
     live = TwitLive(a)
-    live.TrendAnalysis("Worldwide", 5, False)
-    
-    # Test redis
-    # db = redis.Redis()
-    # tweets = a.api.search_tweets("black lives matter",count=10)
-    # for tweet in tweets:
-    #     db.hset(f"tweet:{tweet.id}","text",tweet.text)
-    #     db.hset(f"tweet:{tweet.id}","likes",tweet.favorite_count)
+
+    # Live trend processing
+    live.TopTrendsAnalysis("Worldwide", 10, False)
+
+    for trend in live.trend_streams:
+        trend_document = {
+            "name": trend.name, "volume": trend.volume,
+            "tweets": trend.tweets, "reg_tweets": trend.reg_tweets,
+            "retweets": trend.retweets, "positive_tweets": trend.pos,
+            "negative_tweets": trend.neg, "sentiment": trend.get_sentiment(),
+            "impact": trend.impact_raw, "top_users": [],
+            "top_locations": []
+        }
+
+        # If trend exists, update the DB values
+        if trend.name in trend_list:
+            cur_trend = trends.find_one({"name":trend.name})
+            sentiment = calculate_sentiment(trend, cur_trend["positive_tweets"], cur_trend["negative_tweets"], cur_trend["tweets"])
+            trends.update_one({"name":trend.name},
+                {
+                "$inc": {  "tweets":trend.tweets, 
+                            "reg_tweets": trend.reg_tweets,
+                            "retweets": trend.retweets,
+                            "positive_tweets": trend.pos,
+                            "negative_tweets": trend.neg,
+                            "impact": trend.impact_raw
+                        },
+                "$set": {   "sentiment":sentiment }
+                })
+        # If trend doesn't exist, add it to the DB
+        else:
+            trends.insert_one(trend_document)
+
+
+
+        
